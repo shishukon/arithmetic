799548dc936f9c38f59f971d0a9532702a6b33d5
unknown <hz0xiang@163.com>
2020-03-24 16:44:47 +0800

    Can generate a formula without fraction. No.2

--- src/Get_NewFormula.java
+++ src/Get_NewFormula.java
@@ -5,7 +5,6 @@ public class Get_NewFormula {
         get_Formula formula = new get_Formula(10);
         System.out.println(formula.formula+ "=" + formula.value);
        }
-
     }
 }
 
@@ -13,6 +12,7 @@ public class Get_NewFormula {
 class get_Formula{  //用于组合两个算式
 
     int value;
+ //   String answer;
     String formula;
     Formula f1;
     Formula f2;
@@ -24,23 +24,35 @@ class get_Formula{  //用于组合两个算式
         this.f1 = new Formula(range);
         this.f2 = new Formula(range);
         symbol = all_symbol[(int) (Math.random()*4)];
-        if(f2.value==0&&symbol=='/')
+        if(f2.value==0&&symbol=='/')              //被除数为0
             formula = null;
+
+        ///////////////////////////////////////////////////////////////////////////////
+        //
+        //f1或f2是分式的时运算
+        //示例：
+        //else if(f1.fraction_or_not == 1){ ....; }
+        //
+        ///////////////////////////////////////////////////////////////////////////////
+
+
         else{
-        value = f1.get_value(f1.value, f2.value, symbol); //得到答案
-         if(value < 0){
-             value = -value;
-             formula = this.f2.formula + symbol + this.f1.formula;
-         }
-        else
-            formula = this.f1.formula + symbol + this.f2.formula;
+            value = f1.get_value(f1.value, f2.value, symbol); //得到答案，仅仅是整数   （之后分式功能写完后要分分式的运算和整数的运算）
+
+             if(value < 0){                 //结果为负数，答案取反，调换两个算式的位置
+                 value = -value;
+                 formula = this.f2.formula + symbol + this.f1.formula;
+             }
+            else
+                formula = this.f1.formula + symbol + this.f2.formula;
+
+    //        if (value > (int)value)
+    //            answer = f1.value + "/" + f2.value;
+    //        else
+    //       answer = "" + value ;
 
         }
      }
-
-
-
-
 }
 
 
@@ -52,11 +64,12 @@ class Formula{
     String formula;
     char symbol;
     int swap_or_not = 0;                            //是否出现负数需要调换
+    int fraction_or_not = 0;                        //是否是分式
     public char[] all_symbol = new char[]{'+', '-', '*', '/'};
 
     public  Formula(int range){
         if((Math.random()*2) <=1){            //只有一数的情况
-            value = (int) (Math.random()*(range-1)+1);
+            value = (int)(Math.random()*(range-1)+1);
             formula = String.valueOf(value);
             symbol = '\0';
         }
@@ -88,6 +101,12 @@ class Formula{
                     swap_or_not = 1;
                 return a - b;
             case '/':
+                if (a % b > 0 || a / b == 0) {
+                    fraction_or_not = 1;//是分式，通过分式类存储
+                    //this.fraction[0] = a;     示例
+                    //this.fraction[1] = b;
+                    return 0;
+                }
                 return a / b;
             case '*':
                 return a * b;
