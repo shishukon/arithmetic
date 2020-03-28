dc142415156324c8df76f5bbe2cf2dc975061f72
zzz <1287005870@qq.com>
2020-03-28 15:12:00 +0800

    处理分数

--- src/Get_NewFormula.java
+++ src/Get_NewFormula.java
@@ -3,7 +3,7 @@ public class Get_NewFormula {
     public static void main(String[] args){
         for(int i =0 ;i<=10;i++){
         get_Formula formula = new get_Formula(10);
-        System.out.println(formula.formula+ "=" + formula.value);
+        System.out.println(formula.formula+ "=" + formula.answer);//answer是有分数时的值跟value是没有分数时的值
        }
     }
 }
@@ -13,7 +13,7 @@ class get_Formula{  //用于组合两个算式
 
     int value;
  //   String answer;
-    String formula;
+    String formula,answer;
     Formula f1;
     Formula f2;
     char symbol;
@@ -24,34 +24,47 @@ class get_Formula{  //用于组合两个算式
         this.f1 = new Formula(range);
         this.f2 = new Formula(range);
         symbol = all_symbol[(int) (Math.random()*4)];
-        if(f2.value==0&&symbol=='/')              //被除数为0
-            formula = null;
+        if (f1.fraction_or_not == 0 && f2.fraction_or_not == 0){
+            if(f2.value==0 && symbol=='/')              //除数为0
+                formula = null;
+            else{
+                if (symbol=='/' && f1.value % f2.value != 0){
+                    answer = f1.fraction(f1.value,f2.value);//补充个标记
+                    formula = this.f1.formula + symbol + this.f2.formula;
+                } else{
+                    value = f1.get_value(f1.value, f2.value, symbol); //得到答案，仅仅是整数   （之后分式功能写完后要分分式的运算和整数的运算）
 
-        ///////////////////////////////////////////////////////////////////////////////
-        //
-        //f1或f2是分式的时运算
-        //示例：
-        //else if(f1.fraction_or_not == 1){ ....; }
-        //
-        ///////////////////////////////////////////////////////////////////////////////
-
-
-        else{
-            value = f1.get_value(f1.value, f2.value, symbol); //得到答案，仅仅是整数   （之后分式功能写完后要分分式的运算和整数的运算）
-
-             if(value < 0){                 //结果为负数，答案取反，调换两个算式的位置
-                 value = -value;
-                 formula = this.f2.formula + symbol + this.f1.formula;
-             }
-            else
+                    if(value < 0){                 //结果为负数，答案取反，调换两个算式的位置
+                        value = -value;
+                        formula = this.f2.formula + symbol + this.f1.formula;
+                    }
+                    else
+                        formula = this.f1.formula + symbol + this.f2.formula;
+                }
+            }
+        }else if (f1.fraction_or_not == 1 && f2.fraction_or_not == 1){
+            if (symbol == '-' && f1.value1 * f2.value2 < f2.value1 * f1.value2){
+                formula = this.f2.formula + symbol + this.f1.formula;
+            }else {
                 formula = this.f1.formula + symbol + this.f2.formula;
-
-    //        if (value > (int)value)
-    //            answer = f1.value + "/" + f2.value;
-    //        else
-    //       answer = "" + value ;
-
+            }
+            answer = f1.get_answer(f1.value1,f1.value2,f2.value1,f2.value2,symbol);
+        }else if (f1.fraction_or_not == 1){
+            if (symbol == '-' && f1.value1 * 1 < f2.value * f1.value2){
+                formula = this.f2.formula + symbol + this.f1.formula;
+            }else {
+                formula = this.f1.formula + symbol + this.f2.formula;
+            }
+            answer = f1.get_answer(f1.value1,f1.value2,f2.value,1,symbol);
+        }else{
+            if (symbol == '-' && f1.value * f2.value2 < f2.value1 * 1){
+                formula = this.f2.formula + symbol + this.f1.formula;
+            }else {
+                formula = this.f1.formula + symbol + this.f2.formula;
+            }
+            answer = f1.get_answer(f1.value,1,f2.value1,f2.value2,symbol);
         }
+
      }
 }
 
@@ -61,7 +74,8 @@ class Formula{
     int value;  //数的和，单个数的时候是本身，两个数的时候是两数的运算结果
     int value1; //两个数的时候第一个数
     int value2; //两个数的时候第二个数
-    String formula;
+    int isFu = 0;//isFu是标志是否负数。
+    String formula,fenShu;//fenShu用来存放分数值，formula用来存放算式
     char symbol;
     int swap_or_not = 0;                            //是否出现负数需要调换
     int fraction_or_not = 0;                        //是否是分式
@@ -77,8 +91,17 @@ class Formula{
             value1 = (int)(Math.random()*range + 1);
             value2 = (int)(Math.random()*range + 1);//两个数的情况
             symbol = all_symbol[(int) (Math.random()*4)];
-            value = get_value(value1, value2, symbol);
-            if(symbol == '+'||symbol == '-'){        //加减，需要加括号
+            if (symbol == '/' && value1 % value2 != 0){
+                fraction_or_not = 1;
+                fenShu = fraction(value1 ,value2);
+            }else
+                value = get_value(value1, value2, symbol);
+            if (swap_or_not == 1){
+                value = -value;
+                formula = "(" + value2 + symbol + value1 + ")";
+            }else
+                formula = "(" + value1 + symbol + value2 + ")";
+            /*if(symbol == '+'||symbol == '-'){        //加减，需要加括号
                 if (swap_or_not == 1){
                     value = -value;
                     formula = "(" + value2 + symbol + value1 + ")";}
@@ -86,7 +109,7 @@ class Formula{
                     formula = "(" + value1 + symbol + value2 + ")";
             }
             else                                      //乘除
-                formula =   String.valueOf(value1) + symbol + value2 ;
+                formula =   String.valueOf(value1) + symbol + value2 ;*/
 
         }
 
@@ -101,17 +124,60 @@ class Formula{
                     swap_or_not = 1;
                 return a - b;
             case '/':
-                if (a % b > 0 || a / b == 0) {
-                    fraction_or_not = 1;//是分式，通过分式类存储
+                /*if (a % b > 0 || a / b == 0) {
+                   // fraction_or_not = 1;//是分式，通过分式类存储
                     //this.fraction[0] = a;     示例
                     //this.fraction[1] = b;
                     return 0;
-                }
+                }*/
                 return a / b;
             case '*':
                 return a * b;
         }
         return 0;
+    }
+
+    public String get_answer(int a,int b,int c, int d,char symbol){
+        int fenZi,fenMu;
+        switch (symbol) {
+            case '+':
+                fenZi = a * d + b * c;
+                fenMu = b * d;
+                return fraction(fenZi,fenMu);
+            case '-':
+                fenZi = a * d - b * c;
+                fenMu = b * d;
+                if(fenZi<0) {
+                    isFu = 1;
+                    return fraction(-fenZi,fenMu);
+                }
+                return fraction(fenZi,fenMu);
+
+            case '/':
+                fenZi = a * d;
+                fenMu = b * c;
+                return fraction(fenZi,fenMu);
+            case '*':
+                fenZi = a * c;
+                fenMu = b * d;
+                return fraction(fenZi,fenMu);
+        }
+        return null;
+    }
+
+
+    public String fraction(int a,int b){  //处理分数
+        int zhengShu = a / b, fenZi = a - zhengShu * b,i,maxYinShu = 1;
+        for (i = 1;i <= fenZi;i++){//找最大公因数,化简分数
+            if (fenZi % i == 0 && b % i == 0)
+                maxYinShu = i;
+        }
+        String fenShu;
+        if (zhengShu == 0)
+            fenShu = String.valueOf(fenZi / maxYinShu) + '/' +String.valueOf(b / maxYinShu);
+        else
+            fenShu = String.valueOf(zhengShu) + '’' + String.valueOf(fenZi / maxYinShu) + '/' +String.valueOf(b / maxYinShu);
+        return fenShu;
     }
 
 }
